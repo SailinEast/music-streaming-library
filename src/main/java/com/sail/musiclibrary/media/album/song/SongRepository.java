ecf904324938855b46f91c6ec33145a5811c4c8a
Sail <anasboranbaev@gmail.com>
2026-01-26 22:39:40 +0500

    Bulk deletion optimization

--- src/main/java/com/sail/musiclibrary/media/album/song/SongRepository.java
+++ src/main/java/com/sail/musiclibrary/media/album/song/SongRepository.java
@@ -1,8 +1,13 @@
 package com.sail.musiclibrary.media.album.song;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
+    @Modifying
+    @Query(value = "DELETE FROM playlist_song WHERE song_id = :songId", nativeQuery = true)
+    void removeSongFromAllPlaylists(Long songId);
 }
